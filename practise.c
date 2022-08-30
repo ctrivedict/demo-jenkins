2d7665c4dd6a2d09d7ec13817dd67e6db3497aa7
ctrivedict <112467108+ctrivedict@users.noreply.github.com>
2022-08-30 14:04:57 +0530

    Update practise.c

--- practise.c
+++ practise.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 int main()
 {
-    printf("hi World");
+    int count=0;
+    for(int i=1;i<=5;i++)
+    {
+        for(int k=1;k<=5-i;k++)
+        {
+        printf(" ");
+        }
+        count=0;
+        for(int j=1;j<=i;j++)
+        {
+            count++;
+            printf("%d",count);
+        }
+        for(int j=1;j<i;j++)
+        {
+            count--;
+            printf("%d",count);
+        }
+        printf("\n");
+    }
+   return 0;
 }
